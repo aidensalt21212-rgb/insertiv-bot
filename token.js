d3041070215dadc80cba26930f7861656a2a1bf0
aidensalt21212-rgb <aidensalt21212@gmail.com>
2026-07-27 23:41:02 -0400

    Update token.js

--- token.js
+++ token.js
@@ -3,4 +3,4 @@
 // set the DISCORD_TOKEN environment variable in the Railway project settings.
 // This file is intentionally a harmless placeholder so repositories aren't leaked.
 
-module.exports = 'MTUzMTQ5NTA1NjAxNDMxMTU0Ng.GTHD9K.j3mIw2imrjZdvc0hVgIOu_8IhZz7N82FL5In2U';
+module.exports = '';
